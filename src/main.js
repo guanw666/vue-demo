0390a627256027dad8ca82f1357dde877b33833a
guanw666 <Guanweid1>
2019-09-29 00:24:42 +0800

    完善tabbar

--- src/main.js
+++ src/main.js
@@ -4,10 +4,19 @@ import App from './App.vue';
 import 'mint-ui/lib/style.css';
 import {Header} from 'mint-ui';
 import '../src/lib/mui/css/mui.css';
+import '../src/lib/mui/css/icons-extra.css'
+// 导入路由的包
+import VueRouter from 'vue-router';
+// 安装路由
+Vue.use(VueRouter);
+// 导入自己的路由模块
+import router from './router.js';
 
 Vue.component(Header.name, Header);
 
 let vm = new Vue({
     el: '#app',
-    render: c => c(App)
+    render: c => c(App),
+    // 挂载路由对象到VM实例上
+    router
 });
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import VueRouter from 'vue-router';
 
 let router = new VueRouter({
-    routes: []
+    // 配置路由规则
+    routes: [],
+    // 覆盖默认的路由高亮类
+    linkActiveClass: 'mui-active'
 });
 export default router;
